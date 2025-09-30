0552fbb447841de7531893c08237070001acd913
KirillovLAB <askirillov97@mail.ru>
2025-09-30 16:58:38 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.secret_key = 'your-secret-key-here'  # Нужно для flash сообщений
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/section1')
+@app.route('/section1', methods=['GET', 'POST'])
 def section1():
-    return render_template('section.html', title="Раздел 1", section_id=1)
+    if request.method == 'POST':
+        device_number = request.form.get('device_number')
+        # Здесь можно сохранить данные в базу данных
+        flash(f'Чек-лист ТО-1 для прибора {device_number} сохранен!', 'success')
+    return render_template('section.html', title="ТО-1", section_id=1)
 
-@app.route('/section2')
+@app.route('/section2', methods=['GET', 'POST'])
 def section2():
-    return render_template('section.html', title="Раздел 2", section_id=2)
+    if request.method == 'POST':
+        device_number = request.form.get('device_number')
+        flash(f'Чек-лист ТО-2 для прибора {device_number} сохранен!', 'success')
+    return render_template('section.html', title="ТО-2", section_id=2)
 
-@app.route('/section3')
+@app.route('/section3', methods=['GET', 'POST'])
 def section3():
-    return render_template('section.html', title="Раздел 3", section_id=3)
+    if request.method == 'POST':
+        device_number = request.form.get('device_number')
+        flash(f'Чек-лист ТО-3 для прибора {device_number} сохранен!', 'success')
+    return render_template('section.html', title="ТО-3", section_id=3)
 
-@app.route('/section4')
+@app.route('/section4', methods=['GET', 'POST'])
 def section4():
-    return render_template('section.html', title="Раздел 4", section_id=4)
+    if request.method == 'POST':
+        device_number = request.form.get('device_number')
+        flash(f'Чек-лист ТО-0 для прибора {device_number} сохранен!', 'success')
+    return render_template('section.html', title="ТО-0", section_id=4)
 
 if __name__ == '__main__':
     app.run(debug=True)
